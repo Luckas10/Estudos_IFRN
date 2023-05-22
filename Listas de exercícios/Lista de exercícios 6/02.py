2c45965fbaca2c3fbd73b5e4353afd21e8bcac15
João Lucas <113383724+Luckas10@users.noreply.github.com>
2023-05-22 13:34:23 -0300

    Add files via upload

---
+++ Listas de exercícios/Lista de exercícios 6/02.py
@@ -0,0 +1,24 @@
+# 2) Faça um script Python que solicite a digitação dos elementos (números inteiros) de uma matriz 3 x 3 e, 
+# ao final, exiba quantos e quais valores inseridos são maiores que 10.
+
+matriz = []
+maior_10 = 0
+
+print(f"Informe os elementos da matriz 3x3")
+print("-" * 25)
+for i in range(3):
+    linha = []
+    for j in range(3):
+        valor = int(input(f"Informe o elemento [{i}]x[{j}]: "))
+        if valor > 10:
+            maior_10 += 1
+        linha.append(valor)
+    matriz.append(linha)
+
+print("-" * 25)
+print("Matriz digitada: ")
+for i in range(3):
+    print(matriz[i])
+
+print("-" * 25)
+print(f"Número de elementos maiores que 10: {maior_10}")
