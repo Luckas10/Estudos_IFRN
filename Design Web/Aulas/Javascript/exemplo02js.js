c44bd37f1df4ce61cf716c65e1a0e7bb33878a07
João Lucas <johndescraft@gmail.com>
2023-11-09 14:21:51 -0300

    update

---
+++ Design Web/Aulas/Javascript/exemplo02js.js
@@ -0,0 +1,12 @@
+const div = document.createElement("div");
+
+document.body.appendChild(div);
+
+document.body.style.height = '100vh';
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.body.style.boxSizing = 'border-box';
+
+div.style.display = 'flex';
+div.style.height = '100%';
+div.style.background = 'gray';
