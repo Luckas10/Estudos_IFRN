637389277ff1171a4d066282c1e1d11d32ba6324
João Lucas <johndescraft@gmail.com>
2023-11-30 13:20:40 -0300

    Update questao_03.js

--- Design Web/Listas de exercícios/Lista de exercício 1/questao_03.js
+++ Design Web/Listas de exercícios/Lista de exercício 1/questao_03.js
@@ -1,5 +1,10 @@
-var data = new Date();
-var teste1 = data.toGMTString();
-var teste2 = Data.now();
+var hoje = new Date();
+var data_natal = new Date(hoje.getFullYear, 11, 25);
 
-console.log(teste1)
+if (hoje.getMonth() === 11 && hoje.getDate() > 25) {
+    data_natal.setFullYear(hoje.getFullYear() + 1);
+}
+
+var diferenca = data_natal - hoje;
+
+console.log(diferenca);
